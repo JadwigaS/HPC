89cb1e3860eb80692aa8b5f81304af302cf393fc
JadwigaS <80515572+JadwigaS@users.noreply.github.com>
2024-01-06 18:04:04 +0100

    Create main.c
    sam poczatek

---
+++ main.c
@@ -0,0 +1,21 @@
+
+#define N=1000 //rozmiar siatki
+void walka(){
+    //Todo dodaj losowanie walki
+}
+void więcej(){
+    //todo dodaj rozmnażanie
+}
+int main(){
+  int **kraj;
+    for (int i=0;i<=N;i++){
+        for(int j=0;j<=N;j++){
+            
+            kraj[i][j]=0;
+
+        }
+    }
+
+
+    return 0;
+}
